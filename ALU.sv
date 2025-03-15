a1186ce77950af381980f8fefeab5ef3355dd0bd
GiselleMylHin <167721565+GiselleMylHin@users.noreply.github.com>
2025-03-14 23:01:34 -0700

    Update ALU.sv

--- ALU.sv
+++ ALU.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Engineer: Giselle Hinahon & Zachary Alvarado
+// Engineer: Giselle Hinahon
 // 
 // Create Date: 03/01/2025 12:22:59 PM
 // Module Name: ALU
